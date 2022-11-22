032b5b3de42876a9cec48b079c93313333a49801
Mistly <craftyminer22@gmail.com>
2022-11-22 13:22:05 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,8 @@
+--INSERT INTO sandbox(bool_value, string_value, int32_value) VALUES (0, 'big taco', 987987987);
+
+--UPDATE sandbox SET double_value = 4.2020202 WHERE id = 123;
+--UPDATE sandbox SET string_value = 'big taco perseveres' where id = 123;
+
+--DELETE FROM sandbox where id = 70;
+
+SELECT * FROM sandbox;
